bae993fef2c4912c631621370e9bf928441090f2
Colin <cpillsb1@swarthmore.edu>
2017-05-02 20:23:21 -0400

    starting testing

--- kobe.py
+++ kobe.py
@@ -14,28 +14,42 @@ import csv
 from sklearn.ensemble import *
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-import matplotlib.pyplot as plt
+# from sklearn.ensemble import RandomForestClassifier
+# import matplotlib.pyplot as plt
 # %matplotlib inline
-from sklearn.cross_validation import KFold
+from sklearn.cross_validation import KFold, cross_val_score
+
+
+
 def randomForestStrToNum():
     # turn categorical variables into dummy variables
     categorical_vars = ['action_type', 'combined_shot_type', 'shot_type', 'opponent', 'period', 'season']
     for var in categorical_vars:
         raw = pd.concat([raw, pd.get_dummies(raw[var], prefix=var)], 1)
         raw = raw.drop(var, 1)
+
+
 def main():
     # import data; maybe make new function for preprocessing data
     filename= "data.csv"
     raw = pd.read_csv(filename)
     nona =  raw[pd.notnull(raw['shot_made_flag'])]
-    nona['remaining_time'] = ['minutes_remaining'] * 60 + nona['seconds_remaining']
-    nona["last_5_sec_in_period"] = nona["remaining_time"] < 5
+    raw['remaining_time'] = raw['minutes_remaining'] * 60 + raw['seconds_remaining']
+    raw["last_5_sec_in_period"] = raw["remaining_time"] < 5
     drops = ["minutes_remaining", "seconds_remaining","team_id", "shot_zone_area", 'shot_zone_range', 'shot_zone_basic', "game_date", "team_name", "matchup", "lat", "lon", 'game_event_id']
-    nona["home_play"] = nona["matchup"].str.contains("vs").astype("int")
+    raw["home_play"] = raw["matchup"].str.contains("vs").astype("int")
     for drop in drops:
         nona = nona.drop(drop, 1)
     train = nona.drop('shot_made_flag', 1)
     train_y = nona['shot_made_flag']
-    
+
+    print(nona)
+
+    model = RandomForestClassifier(n_estimators=10)
+    model = model.fit(train, train_y)
+    results = cross_val_score(model, train, train_y)
+    print results
+
+
+
 main()
